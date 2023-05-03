db4d792b3bc6053ae28e855aa02eb8e74d05e706
satyajitnayk <satyajitn15@gmail.com>
2023-05-03 22:57:15 +0530

    Add search functionality for restaurants.

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,22 +5,48 @@ import { useState } from 'react';
 const Body = () => {
   // Local state variable
   const [restaurantList, setRestaurantList] = useState(restaurants);
+  const [searchText, setSearchText] = useState('KFC');
 
   return (
     <div className="body">
-      <div className="filter">
-        <button
-          className="filter-btn"
-          onClick={() => {
-            const filteredList = restaurants.filter(
-              (res) => res.data.avgRating > 4
-            );
-            setRestaurantList(filteredList);
-          }}
-        >
-          Top Rated Restaurants
-        </button>
+      <div className="search-filter-container">
+        <div className="search-container">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              const filteredList = restaurants.filter((res) =>
+                res.data.name.includes(searchText)
+              );
+              setRestaurantList(filteredList);
+            }}
+          >
+            search
+          </button>
+        </div>
+        <div className="filter">
+          <button
+            className="filter-btn"
+            onClick={() => {
+              const filteredList = restaurants.filter(
+                (res) => res.data.avgRating > 4
+              );
+              setRestaurantList(filteredList);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
       </div>
+
       <div className="res-container">
         {/* Restaurant Card */}
         {restaurantList.map((restaurant) => (
